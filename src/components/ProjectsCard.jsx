405b3e7a2f665c89957ef0faf70843981ed67b6c
Djordje <djolej17@gmail.com>
2024-05-23 23:51:28 +0200

    Project card - link image

--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -5,11 +5,13 @@ import { TbWorldWww } from 'react-icons/tb'
 const ProjectsCard = ({ img, url, github, title, text, tech, note }) => {
   return (
     <article className='bg-white rounded-lg shadow-md hover:shadow-xl duration-300'>
-      <img
-        src={img}
-        className='w-full object-cover rounded-t-lg h-64'
-        alt={title}
-      />
+      <a href={url} target='_blank' rel='noopener noreferrer'>
+        <img
+          src={img}
+          className='w-full object-cover rounded-t-lg h-64'
+          alt={title}
+        />
+      </a>
       <div className='capitalize p-8'>
         <h2 className='text-xl tracking-wide font-medium flex flex-row items-center gap-5'>
           {title}
@@ -26,7 +28,7 @@ const ProjectsCard = ({ img, url, github, title, text, tech, note }) => {
         </div>
       </div>
 
-      <div className='align-element'>{note}</div>
+      <div className='align-element text-cyan-600 pb-4'>{note}</div>
     </article>
   )
 }
